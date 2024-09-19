c1c0972cba921b629c81e706e2c72f0fdeed7b11
Shurik3953 <sasharudenkooo06@gmail.com>
2024-09-19 16:39:09 +0300

    Create 7x7.cpp

---
+++ 7x7.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+int number = 7;
+cout << "Таблиця множення для числа " <<number << ":" << endl;
+for (int i=1; i <=10; i++) {
+cout << number << "*" << i << "=" << number * i << endl ;}
+}
